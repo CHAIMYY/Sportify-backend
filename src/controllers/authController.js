6f6af6b30fd81efbc763dcfdb935cd8f48ff8db5
Chaimaa HOUAS <144816252+CHAIMYY@users.noreply.github.com>
2024-11-26 15:59:41 +0100

    SPOR-52 feat: added login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -17,3 +17,23 @@ exports.register = async function (req, res) {
     return res.status(400).send({ message: err.message });
   }
 };
+
+exports.login = async function (req, res) {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user || !user.comparePassword(req.body.password)) {
+      return res.status(401).json({ message: 'Authentication failed. Invalid user or password.' });
+    }
+
+    const token = jwt.sign(
+      { email: user.email, nom: user.lastname, prenom: user.firstname, _id: user._id },
+      process.env.JWT_SECRET || "RESTFULAPIs"
+    );
+
+    return res.json({ token });
+
+  } catch (err) {
+    return res.status(500).send({ message: 'Error in authentication' });
+  }
+};
